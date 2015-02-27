8044a784769fd4aa2aa9c10e561c55e0d5c2b3a6
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-26 22:50:52 -0600

    Added rack cors configurations

--- chat_app.rb
+++ chat_app.rb
@@ -3,5 +3,62 @@ require 'bundler/setup'
 Bundler.require
 
 class ChatApp < Sinatra::Base
+  
+
+  configure :development do
+    REDIS = Redis.new
+    use Rack::Cors do
+      allow do
+        origins 'ws://localhost:9292', 'ws://127.0.0.1:9292', 'ws://localhost:9393', 'ws://127.0.0.1:9393'
+        resource '*'
+      end
+    end
+  end
+
+  configure :production do
+
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+    use Rack::Cors do
+      allow do
+        origins 'ws://city-gram.herokuapp.com'
+                # regular expressions can be used here
+          resource '*'
+        end
+    end
+  end
+
+  set :redis, REDIS
+  set(:watcher, Thread.new do
+    redis = Redis.new
+    Thread.current['sockets'] = []
+    redis.subscribe 'chat_screen' do |on|
+      on.message do |channel, message|
+        Thread.current['sockets'].each do |s|
+          s.send message
+        end
+      end
+    end
+  end)
+
+  get '/' do
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send( {'sender' => 'CityGram', message: 'Give a Shout Out!'}.to_json )
+        settings.watcher['sockets'] << ws
+      end
+
+      ws.onmessage do |msg|
+        settings.redis.publish 'chat_screen', msg
+      end
+
+      ws.onclose do
+        warn("websocket closed")
+        settings.watcher['sockets'].delete(ws)
+        # settings.sockets.delete(ws)
+      end
+    end
+  end
 
 end
